3881b6e5f4062e3c6c5161ccc2b35fc77133bc90
Daehyun Kim <hatemogi@gmail.com>
2016-04-14 23:49:33 +0900

    1st practice using instaparse

--- src/instaparse_practice/core.clj
+++ src/instaparse_practice/core.clj
@@ -1,6 +1,31 @@
-(ns instaparse-practice.core)
+(ns instaparse-practice.core
+  (:require [instaparse.core :as insta]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def md-parser
+  (insta/parser
+   "doc             := {section | para}
+    section         := heading para
+    blank-line      := <WP* NL>
+    <heading>       := h1 | h2
+    <heading-title> := para
+    h1              := <'#'> <WP*> para <['#']>
+    h2              := <'#'> h1 <['#']>
+    para            := (letter | blank-line | WP)+
+
+    <letter>   := #'[a-zA-Z0-9]'
+    <WP>       := #'\\s'
+    <NL>       := ['\\r'] '\\n'
+    "))
+
+(def test-parser
+  (insta/parser
+   "doc        := para
+    para       := letter+
+
+    <letter>   := #'[a-zA-Z0-9]'
+    <WP>       := #'\\s'
+    <NL>       := ['\\r'] '\\n'
+    "))
+
+(md-parser "# abc \nabc\n \nab")
+(test-parser "test")
