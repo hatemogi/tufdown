fc7b217be83e3307134e874a5c3d953cabeccb11
Daehyun Kim <hatemogi@gmail.com>
2016-04-15 00:30:18 +0900

    add reference links

--- doc/intro.md
+++ doc/intro.md
@@ -1,3 +1,8 @@
-# Introduction to instaparse-practice
+# References
 
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
+* https://github.com/Engelberg/instaparse/blob/master/test/instaparse/core_test.clj
+* https://github.com/Engelberg/instaparse
+* https://github.com/chameco/Hitman/blob/master/src/hitman/core.clj
+* https://github.com/jbranchaud/js-play/blob/master/Markdown/grammar.md
+* https://github.com/jgm/peg-markdown/blob/master/markdown_parser.leg
+* https://en.wikipedia.org/wiki/Parsing_expression_grammar
--- src/instaparse_practice/core.clj
+++ src/instaparse_practice/core.clj
@@ -3,19 +3,22 @@
 
 (def md-parser
   (insta/parser
-   "doc             := {section | para}
-    section         := heading para
-    blank-line      := <WP* NL>
-    <heading>       := h1 | h2
+   "doc             := section*
+    section         := heading? block+
+    block           := para | blank-line
+    para            := WORD
+    blank-line      := [WP] EOL
+    <heading>       := h1
     <heading-title> := para
-    h1              := <'#'> <WP*> para <['#']>
-    h2              := <'#'> h1 <['#']>
-    para            := (letter | blank-line | WP)+
+    h1              := <'#'> [WP] heading-title [WP] ['#']
 
-    <letter>   := #'[a-zA-Z0-9]'
-    <WP>       := #'\\s'
-    <NL>       := ['\\r'] '\\n'
+    <WORD>     := #'\\S+'
+    WP         := (' ' | '\\t')+
+    EOL        := <['\\r'] '\\n'>
     "))
+
+(md-parser "# title한글\n")
+(md-parser "# 한글제목\nabc\n \nab")
 
 (def test-parser
   (insta/parser
@@ -23,9 +26,9 @@
     para       := letter+
 
     <letter>   := #'[a-zA-Z0-9]'
-    <WP>       := #'\\s'
+    <WP>       := #'\\s+'
     <NL>       := ['\\r'] '\\n'
     "))
 
-(md-parser "# abc \nabc\n \nab")
+
 (test-parser "test")
