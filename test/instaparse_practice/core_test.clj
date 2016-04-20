f9a78f782aebae6234164a4f73745ae3759414f3
Daehyun Kim <hatemogi@gmail.com>
2016-04-20 23:13:15 +0900

    강조까지 구현

--- test/instaparse_practice/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns instaparse-practice.core-test
-  (:require [clojure.test :refer :all]
-            [instaparse-practice.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
