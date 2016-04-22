b634f34e22f02714a50cafa9652035d4d42b6d89
Daehyun Kim <hatemogi@gmail.com>
2016-04-22 23:41:28 +0900

    클로저스크립트 라이닝언 플러그인 추가

--- project.clj
+++ project.clj
@@ -7,4 +7,11 @@
                  [instaparse "1.4.1"]
                  [org.clojure/clojurescript "1.8.40"]
                  [com.lucasbradstreet/instaparse-cljs "1.4.1.1"]]
+  :plugins [[lein-cljsbuild "1.1.3"]
+            [lein-figwheel "0.5.2"]]
+  :cljsbuild
+  {:builds [{:source-paths ["src-cljs"]
+             :compiler {:output-to "public/js/main.js"
+                        :optimizations :whitespace
+                        :pretty-print true}}]}
   :test-paths ["test" "src"])
