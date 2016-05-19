ff0974af066e16db139d2f9eddc804e9240e1502
Daehyun Kim <hatemogi@gmail.com>
2016-05-19 14:09:26 +0900

    프로젝트 웹사이트 주소 수정

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject tufdown "0.1.0-SNAPSHOT"
   :description "a markdown-like language and parser for TufteCSS"
-  :url "https://github.com/hatemogi/instaparse-practice"
+  :url "https://github.com/hatemogi/tufdown"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
